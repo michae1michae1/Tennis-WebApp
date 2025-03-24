c28128775c647b96a9cb9bf80ce660085f6abec2
michae1michae1 <j.braden117@gmail.com>
2025-03-23 22:43:30 -0400

    update config for loading assets

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/Tennis-WebApp' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/Tennis-WebApp/' : '',
   images: {
     domains: ['localhost', 'images.unsplash.com'],
     unoptimized: true, // Required for static export
